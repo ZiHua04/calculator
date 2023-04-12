54d24bb5754ff7b3ad201f91986d91e708a15c4b
ZiHua04 <1227097374@qq.com>
2023-04-12 23:19:09 +0800

    1

--- zzh-workspace/hello/src/hello/Helloworld.java
+++
@@ -1,9 +0,0 @@
-package hello;
-
-public class Helloworld {
-    public static void main(String[] args)
-    {
-    	MyClass a = new MyClass();
-    	a.cout(15);
-    }
-}
--- zzh-workspace/hello/src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author zzzh
- *
- */
-module hello {
-}
