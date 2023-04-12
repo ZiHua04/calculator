026ed1970b9fc41514c08b327fc2e15a8755474b
ZiHua04 <1227097374@qq.com>
2023-04-12 15:31:02 +0800

    Update Main.java
    test code

--- zzh-workspace/calculator/calculator/Main.java
+++ zzh-workspace/calculator/calculator/Main.java
@@ -1,8 +1,27 @@
 package calculator;
 
-public class Main {
-	static public void main(String[] args)
-	{
-		System.out.println("Hello");
-	}
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Main extends JFrame {
+    public Main() {
+        setTitle("JButton Example");
+        setSize(300, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JButton button = new JButton("Click Me"); // 创建一个按钮
+        button.addActionListener(new ActionListener() { // 添加按钮的事件监听器
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "Hello World"); // 弹出对话框并显示消息
+            }
+        });
+
+        add(button); // 将按钮添加到窗口中
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Main();
+    }
 }
