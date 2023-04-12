9368d8708e05b81ed3d371e1f967729ec479330e
ZiHua04 <128723416+ZiHua04@users.noreply.github.com>
2023-04-12 14:37:55 +0800

    Add files via upload

---
+++ zzh-workspace/hello/src/hello/Helloworld.java
@@ -0,0 +1,9 @@
+package hello;
+
+public class Helloworld {
+    public static void main(String[] args)
+    {
+    	MyClass a = new MyClass();
+    	a.cout(15);
+    }
+}
---
+++ zzh-workspace/hello/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author zzzh
+ *
+ */
+module hello {
+}
